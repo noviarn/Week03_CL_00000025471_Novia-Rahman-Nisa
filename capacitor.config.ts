032b4ceb9ecc3169347f90db70c943862d201c34
noviarn <42713448+noviarn@users.noreply.github.com>
2021-09-16 21:34:59 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Week03_CL_00000025471_Novia Rahman Nisa',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
